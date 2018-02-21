26f0fc2b9e72bc190364e0b47e1fd760d890b195
Avera123 <avera02@misena.edu.co>
2018-02-21 14:36:32 -0500

    listado de casos

--- src/Controller/CasoController.php
+++ src/Controller/CasoController.php
@@ -4,7 +4,9 @@ namespace App\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 
 class CasoController extends Controller
 {
@@ -15,20 +17,18 @@ class CasoController extends Controller
     {
         // Get cURL resource
         $curl = curl_init();
-// Set some options - we are passing in a useragent too here
         curl_setopt_array($curl, array(
             CURLOPT_RETURNTRANSFER => 1,
 //            CURLOPT_URL => 'https://my-json-server.typicode.com/Avera123/jsonserver/usuarios',
-            CURLOPT_URL => 'http://oro.avera.com/app_dev.php/api/get/casos',
+            CURLOPT_URL => 'http://oro.avera.com/app_dev.php/api/lista/casos/1',
         ));
-// Send the request & save response to $resp
         $resp = curl_exec($curl);
-// Close request to clear up some resources
         curl_close($curl);
 
         $respuesta = json_decode($resp);
-// replace this line with your own code!
-        return $this->render('Pages/PanelDeControl.html.twig', array(
+//dump($respuesta);
+//die();
+        return $this->render('Caso/listar.html.twig', array(
             'casos' => $respuesta
         ));
     }

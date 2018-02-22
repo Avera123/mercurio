879de2cc11f99f910683004b4085e1a77a07aac5
Avera123 <avera02@misena.edu.co>
2018-02-22 10:31:25 -0500

    se captura la info del usuario para filtrar por la empresa correspondiente como parametro en el get a la api oro

--- src/Controller/CasoController.php
+++ src/Controller/CasoController.php
@@ -7,21 +7,23 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class CasoController extends Controller
 {
     /**
      * @Route("/caso/listar", name="casoListar")
      */
-    public function index()
+    public function index(UserInterface $user)
     {
         // Get cURL resource
-	    $user = $this->getUser();
+
+
         $curl = curl_init();
         curl_setopt_array($curl, array(
             CURLOPT_RETURNTRANSFER => 1,
 //            CURLOPT_URL => 'https://my-json-server.typicode.com/Avera123/jsonserver/usuarios',
-            CURLOPT_URL => 'http://oro.juan.com/app_dev.php/api/lista/casos/'.$user->getCodigoEmpresaFk(),
+            CURLOPT_URL => 'http://oro.juan.com/app_dev.php/api/lista/casos/'.$user->getCodigoClienteFk(),
         ));
         $resp = curl_exec($curl);
         curl_close($curl);

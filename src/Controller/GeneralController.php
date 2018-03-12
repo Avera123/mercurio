8a79a940964d8bdbeeffc63c20e38105c4012d60
Mario A. Estrada Zuluaga <maestradaz3@gmail.com>
2018-03-12 14:05:30 -0500

    fix: Se corrige el metodo de descarga de los archivos

--- src/Controller/GeneralController.php
+++ src/Controller/GeneralController.php
@@ -9,38 +9,36 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class GeneralController extends Controller
 {
     /**
-     * @Route("/archivo/descargar/{tipo}/{tamano}/{nombre}", requirements={"tipo":"[a-zA-Z0-9./]+","tamano":"\d+","nombre":"[a-zA-Z0-9. ]+"}, name="archivo_descarga")
+     * @Route("/archivo/descargar/{codigoArchivo}", name="archivo_descarga")
      */
-    public function descargar($tipo = null, $tamano = 0, $nombre = null)
+    public function descargar($codigoArchivo)
     {
-//        $em = $this->getDoctrine()->getManager();
-//        $arArchivo = $em->getRepository('App:Archivo')->find($numero);
-//        $strRuta = $arArchivo->getDirectorioRel()->getRutaPrincipal() . $arArchivo->getDirectorioRel()->getNumero() . "/" . $arArchivo->getCodigoArchivoPk() . "_" . $arArchivo->getNombre();
-        $strRuta = '/var/www/archivosoro/';
+        $em = $this->getDoctrine()->getManager(); // instancia el entity manager
+        $serviceUrl = $em->getRepository('App:Configuracion')->getUrl();
+        // Get cURL resource
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_URL => $serviceUrl . 'archivo/descargar/' . $codigoArchivo,
+        ));
+        $arrArchivo = json_decode(curl_exec($curl));
+        $arrArchivo = $arrArchivo[0];
+        curl_close($curl);
+
+        $strRuta = "/var/www/archivosoro/1/" . $arrArchivo->nombreAlmacenamiento;
         // Generate response
         $response = new Response();
-//        $curl = curl_init();
-//        curl_setopt_array($curl, array(
-//            CURLOPT_RETURNTRANSFER => 1,
-//            CURLOPT_URL => 'caso/lista/' . $this->getUser()->getCodigoClienteFk(),
-//        ));
-//        $resp = json_decode(curl_exec($curl));
-//        curl_close($curl);
         // Set headers
         $response->headers->set('Cache-Control', 'private');
-        #$response->headers->set('Content-Type', $tipo);
-        $response->headers->set("Content-Type", "image/jpeg");
-        $response->headers->set('Content-Disposition', 'attachment; filename="' . $nombre . '";');
-        $response->headers->set('Content-Length', $tamano);
-//        $response->setContent(readfile($strRuta));
-//        var_dump($tipo);
-//        exit();
+        $response->headers->set('Content-type', $arrArchivo->tipo);
+        $response->headers->set('Content-Disposition', 'attachment; filename="' . $arrArchivo->nombre . '";');
+        $response->headers->set('Content-length', $arrArchivo->tamano);
+        $response->sendHeaders();
         if(file_exists ($strRuta)){
-            $response->setContent(file_get_contents($strRuta));
+            $response->setContent(readfile($strRuta));
         }else{
             echo "<script>alert('No existe el archivo en el servidor a pesar de estar asociado en base de datos, por favor comuniquese con soporte');window.close()</script>";
         }
-        $response->sendHeaders();
         return $response;
     }
 }
